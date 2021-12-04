1db9bf6016c216912513eae1d38e8ec199e04991
Riyadh-kh <alkharrat.riyadh@gmail.com>
2021-12-04 14:45:21 +0400

    Fixed Data Memory

--- Data_Memory.vhd
+++ Data_Memory.vhd
@@ -15,19 +15,17 @@ end Data_Memory;
 
 architecture Behavioral of Data_Memory is
 
-signal ram_addr: std_logic_vector(29 downto 0);
 type data_mem is array (0 to 15) of std_logic_vector (31 downto 0);
 signal RAM: data_mem :=((others=> (others=>'0')));
 
 begin
-	ram_addr <= mem_access_addr(31 downto 2);
+	mem_read_data <= RAM((to_integer(unsigned(mem_access_addr))- 268435456)/4) when mem_read='1' else x"00000000";
+	
 process(clk)
 	begin
-	if(rising_edge(clk)) then
 	if (mem_write_en='1') then
-		RAM(to_integer(unsigned(ram_addr))) <= mem_write_data;
-	end if;
+		RAM((to_integer(unsigned(mem_access_addr))- 268435456)/4) <= mem_write_data;
 	end if;
 end process;
-		mem_read_data <= RAM(to_integer(unsigned(ram_addr))) when (mem_read='1') else x"00000000";
+
 end Behavioral;
